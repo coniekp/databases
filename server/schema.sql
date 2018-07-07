e9891142c9bfd5ca7c963493c340eef5a4893c14
coniekp <coniekp@icloud.com>
2018-07-06 18:42:20 -0700

    set up table creation in db .sql

--- server/schema.sql
+++ server/schema.sql
@@ -3,8 +3,19 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  objectId INT (10) NOT NULL AUTO_INCREMENT,
+  username VARCHAR(10) ,
+  roomname VARCHAR(10) , 
+  createdAt TIMESTAMP  ,
+  text VARCHAR(180) ,
+  updatedAt TIMESTAMP NULL,
+  PRIMARY KEY (`objectId`)
 );
+
+CREATE TABLE texts (
+  textID int(10) NOT NULL AUTO_INCREMENT,
+  FOREIGN KEY ('objectId')
+)
 
 /* Create other tables and define schemas for them here! */
 
